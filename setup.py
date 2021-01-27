3cb181236ea20ad3459eecc1f52188ead33ecd17
Jim Robinson-Bohnslav <jbohnslav@gmail.com>
2021-01-27 16:51:31 -0500

    fixed conversion; incremented version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setuptools.setup(
     name='vidio',
-    version='0.0.2',
+    version='0.0.3',
     author='Jim Bohnslav',
     author_email='jbohnslav@gmail.com',
     description='Video file IO',
