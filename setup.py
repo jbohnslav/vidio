9795924a4de06a36d53e9827825ee5331ea59db4
Jim Robinson-Bohnslav <jbohnslav@gmail.com>
2022-06-02 20:35:07 -0400

    increment version; headless not uploaded to pypi

--- setup.py
+++ setup.py
@@ -3,22 +3,18 @@ import setuptools
 with open("README.md", 'r') as f:
     long_description = f.read()
 
-setuptools.setup(
-    name='vidio',
-    version='0.0.3',
-    author='Jim Bohnslav',
-    author_email='jbohnslav@gmail.com',
-    description='Video file IO',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    include_package_data=True,
-    packages=setuptools.find_packages(),
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent'
-    ],
-    python_requires='>=3.6',
-    install_requires=['h5py',
-                      'numpy',
-                      'opencv-python-headless']
-)
+setuptools.setup(name='vidio',
+                 version='0.0.4',
+                 author='Jim Bohnslav',
+                 author_email='jbohnslav@gmail.com',
+                 description='Video file IO',
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
+                 include_package_data=True,
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                     'Programming Language :: Python :: 3',
+                     'Operating System :: OS Independent'
+                 ],
+                 python_requires='>=3.6',
+                 install_requires=['h5py', 'numpy', 'opencv-python-headless'])
